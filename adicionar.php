5a4a4b5edb12a79bb5af087a59637e786f408310
Romario Alves <Romariogn10@gmail.com>
2021-04-04 19:12:43 -0300

    sistema controle de usuários com front-end finalizado e finalização da versão 2.0

--- adicionar.php
+++ adicionar.php
@@ -1,5 +1,6 @@
 <?php
 require ('config.php');
+
 if(isset($_POST['nome']) && empty($_POST['nome']) == false){
     $nome = addslashes($_POST['nome']);
     $email = addslashes($_POST['email']);
@@ -20,13 +21,53 @@ if(isset($_POST['nome']) && empty($_POST['nome']) == false){
 }
 
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <title>Cadastrar usuário</title>
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css"> 
+    </head>
+    <body>
+        <div class="container">
+        <h3 class="text-center">Cadastrar novo usuário</h3>
+            <div class="row d-flex justify-content-center ">
+                <div class="col-sm-6 m-10 border border-secondary rounded">
+                    <form method="POST">
+                        <div class="bordered">
+                            <div class="form-group">
+                                <label class="form-label mt-3" for="nome">Nome:</label> 
+                                <input id="nome" type="text" name="nome" class="form-control">
+                            </div>
+                        
+                            <div class="form-group">
+                                <label class="form-label" for="email">E-mail:</label> 
+                                <input id="email" type="text" name="email" class="form-control">
+                            </div>
 
-<form method="POST">
-    Nome:<br>
-    <input type="text" name="nome"><br><br>
-    Email:<br>
-    <input type="text" name="email"><br><br>
-    Senha:<br>
-    <input type="password" name="senha"><br><br>
-    <input type="submit" value="Cadastrar"/>
-</form>
+                            <div class="form-group">
+                                <label class="form-label" for="senha">Senha:</label> 
+                                <input id="senha" type="password" name="senha" class="form-control"> 
+                            </div>
+                            <div class="row d-flex justify-content-between">
+                            <input type="submit" value="Cadastrar" class="btn btn-success mb-3 mr-3 ml-3"/>
+                            <a href="voltar.php" class="btn btn-primary mb-3 mr-3">Voltar</a>
+                            
+                            </div>
+                            
+                        </div>
+                    </form>
+                </div>
+            </div>  
+        </div>
+        
+        <script type="text/javascript" src="assets/js/jquery-3.6.0.min.js"></script>
+        <script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
+
+
+
+
--- index.php
+++ index.php
@@ -1,29 +1,55 @@
 <?php
     require 'config.php';
 ?>
-<a href="adicionar.php">Adicionar Novo Usuário</a>
-<h1>Esse é o novo branch</h1>
-<table border="0" width="100%">
-    <tr>
-        <th>Nome</th>
-        <th>E-mail</th>
-        <th>Ações</th>
-    </tr>
-    <?php
-        $sql = "SELECT * FROM tb_curso";
-        $sql = $pdo->query($sql);
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <title>Controle de usuários</title>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="alert alert-info alert-dismissible fade show" role="alert">
+               Sistema de controle de usuários utilizando PHP e Bootstrap
+                <button class="close" data-dismiss="alert" >
+                    <span aria-hidden="true">&times;</span>   
+                </button>
+           </div>
 
-        if($sql->rowCount() > 0){
-            foreach($sql->fetchAll() as $usuario){
-                echo '<tr>';
-                echo '<td>'.$usuario['nome'].'</td>';
-                echo '<td>'.$usuario['email'].'</td>';
-                echo '<td><a href="editar.php?id='.$usuario['id'].'">Editar</a> - <a href="excluir.php?id='.$usuario['id'].'">Excluir</a></td>';
-                echo '</tr>';
-                
-            }
-        }
-    ?>
+            <div class="table-responsive">
+                <table border="0" width="100%" class="table table-bordered table-hover">
+                    <thead class="thead-light">
+                        <tr>
+                            <th>Nome</th>
+                            <th>E-mail</th>
+                            <th>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            $sql = "SELECT * FROM tb_curso";
+                            $sql = $pdo->query($sql);
 
+                            if($sql->rowCount() > 0){
+                                foreach($sql->fetchAll() as $usuario){
+                                    echo '<tr>';
+                                    echo '<td>'.$usuario['nome'].'</td>';
+                                    echo '<td>'.$usuario['email'].'</td>';
+                                    echo '<td><a href="editar.php?id='.$usuario['id'].'" class="btn btn-info">Editar</a> - <a href="excluir.php?id='.$usuario['id'].'" class="btn btn-danger">Excluir</a></td>';
+                                    echo '</tr>';
+                                }
+                            }
+                        ?>
+                    </tbody> 
+                </table>
+            </div>
+            <a href="adicionar.php" class="btn btn-primary mt-3 mb-3 ">Adicionar Novo Usuário</a>
+        </div>
+        <script type="text/javascript" src="assets/js/jquery-3.6.0.min.js"></script>
+        <script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
 
-</table>
